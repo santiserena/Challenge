4cda17bf9ec2ab7f3ed73bc9e2c8626b784b7eac
santiserena <santiserena@outlook.com.ar>
2022-10-07 11:11:19 -0300

    cleaning format

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-//DESINSTALAR const cookieParser = require('cookie-parser');
-//const bodyParser = require('body-parser');
-//const morgan = require('morgan');
-
 // TO RUN-->  npm start
 
 require('dotenv').config();
@@ -10,39 +6,17 @@ const mongoose = require('mongoose')
 const userRoutes = require('./routes/routes.js')
 var responseTime = require('response-time')
 
-var StatsD = require('node-statsd')
-
-var stats = new StatsD()
-
-
 var app = express();
 
 const port = 3000;
 app.listen(port, () => console.log("Listening on port", port));
 
 //MIDDLEWARES
-
-/* app.use(responseTime((req, res, time) => {
-    console.log(time);
-    req.headers.delay = time
-    //res.setHeader('late', time)
-    console.log('ESTOS SON LOS HEADERS DEL MIDDLEWARE-> ',req.headers);
-})) */
-
 app.use(responseTime())
-
-/* app.use((req, res, next) =>{
-    req.headers.delay2 = 'hola'
-    next();
-  }) */
-  
 app.use(express.json())
 app.use("/api", userRoutes);
-
 
 mongoose
 .connect(process.env.MONGODB_URI)
 .then(() => console.log("Connect to MongoDB Atlas"))
 .catch((error) => console.log(error));
-
-//export default app;
--- routes/routes.js
+++ routes/routes.js
@@ -1,65 +1,57 @@
-const express = require('express')
-const axios = require('axios')
-const mongoose = require('mongoose')
-const Cities = require ("../model/cities"); 
-const { request } = require('express');
-var responseTime = require('response-time')
-
+const express = require("express");
+const axios = require("axios");
+const mongoose = require("mongoose");
+const Cities = require("../model/cities");
 
 const router = express.Router();
 
-//show all cities
-router.get('/allcities', async (req, res) => {
-    //console.log('ACAA HEADERS-------> ',req.headers);
-    const allCities = await Cities.find() 
-    res.send (allCities)
-    
-})
+//show all db cities
+/* router.get("/allcities", async (req, res) => {
+  const allCities = await Cities.find();
+  res.send(allCities);
+}); */
 
-//Gives cities temperature
 router.get("/:city", async (req, res) => {
- 
-
   let succes = false;
   let err = 0;
 
   while (succes === false && err < 3) {
-      try {
-      //possibility of error 15%
-      if (Math.round(Math.random() * 100) <= 15) {
-        err++
-        console.log('primer error');
-        throw new Error("Artificial error -> 15% ");
-      }
-
+    try {
       const alreadyExist = await Cities.find({ name: req.params.city });
       if (alreadyExist.length) {
         succes = true;
-        res.send(alreadyExist);
-
+        res.send(`The temperature consulted is ${alreadyExist[0].temperature} degrees. Source: DB`);
       } else {
-        let info = await axios.get(
-          `https://api.openweathermap.org/data/2.5/weather?q=${req.params.city}&appid=${process.env.API_KEY}&units=metric`
-        );
+        //possibility of error 15%:
+        if (Math.round(Math.random() * 100) <= 15) {
+          err++;
+          console.log("Error counter ->", err);
+          throw new Error("Simulated bug -> 15% ");
+        } 
+        else {
+          
+          let info = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${req.params.city}&appid=${process.env.API_KEY}&units=metric`);
+          let temperature = info.data.main.temp.toString();
 
-        let temperature = info.data.main.temp.toString();
-
-        if (temperature) {
-          let saved = await Cities.create({
-            name: req.params.city,
-            temperature: parseFloat(temperature),
-          });
-          succes = true;
-          res.send("guardado -> " + saved);
+          if (temperature) {
+            let saved = await Cities.create({
+              name: req.params.city,
+              temperature: parseFloat(temperature),
+            });
+            succes = true;
+            res.send(`The temperature consulted is ${saved.temperature} degrees. Source: API weather`);
+          }
         }
       }
     } catch (error) {
-      
-      console.log('van errores->' , err);
-      err++;
+      console.log(error);
     }
-}
-      if (succes===false && err >= 3) res.status(400).send('ERROR');
+  }
+  if (succes === false && err >= 3)
+    res
+      .status(400)
+      .send(
+        "ERROR: There were three failures in the external api, or the city does not exist"
+      );
 });
- 
-module.exports = router 
+module.exports = router;
